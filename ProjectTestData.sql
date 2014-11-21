1159ea3f17e59eb52ede2bbe9f198d6e80f2e62d
Zilun Peng <zilunpeng@gmail.com>
2014-11-21 00:43:35 -0800

    domain constraint and minor refinement

--- ProjectTestData.sql
+++ ProjectTestData.sql
@@ -4,115 +4,265 @@ create database ams;
 
 use ams;
 
+
 drop table if exists item;
-
 create table item
-	(upc char(37) not null,
-	title varchar(40) not null,
-	type varchar(20) not null,
-	category varchar(20) not null,
-	company varchar(20) not null,
-	year integer not null,
-	price float not null,
-	stock integer not null);
- 
-drop table if exists leadsinger;
+	(upc char(3) not null,
+	title varchar(40),
+	type varchar(20),
+	category varchar(20),
+	company varchar(20),
+	year integer,
+	price float,
+	stock integer,
+    primary key (upc),
+    check (type = 'rock' or type = 'pop' or type = 'rap' or type = 'country' or type = 'country' or type = 'classical' or type = 'new age' or type = 'instrumental'));
 
-create table leadsinger
-	(upc char(37) not null,
+ 
+drop table if exists leadSinger;
+create table leadSinger
+	(upc char(3) not null,
 	sname varchar(40) not null,
-	PRIMARY KEY (upc, sname));
- 
- 
-drop table if exists customer;
- 
-create table customer
-	(cid varchar(40) not null,
-	name varchar(40) not null,
-	password varchar(40) not null,
-	phone varchar(12),
-	address varchar(40));
- 
- 
-drop table if exists purchase;
- 
+	primary key (upc, sname));
+
+
+drop table if exists hasSong;
+create table hasSong 
+	(upc char(3) not null,
+	 title varchar(40) not null,
+     primary key (upc, title));
+   
+   
+drop table if exists purchase; 
 create table purchase
-	(receiptId int(10) PRIMARY KEY AUTO_INCREMENT,
-	purchaseDate date not null,
-	cid varchar(40) not null,
-	cardNumber int(16) not null,
-	expiryDate date not null,
-	expectedDate date not null,
-	deliveredDate date);
+	(receiptId int(10) not null auto_increment,
+	purchaseDate date,
+	cid varchar(40),
+	cardNumber int(16),
+	expiryDate date,
+	expectedDate date,
+	deliveredDate date,
+	primary key (receiptId));
  
  
 drop table if exists purchaseItem;
- 
-create table purchaseItem
-	(receiptId int(10),
+create table purchaseItem 
+	(receiptId int(10)  not null,
 	upc char(37) not null,
-	quantity int not null,
-	PRIMARY KEY(receiptId, upc));
+	quantity int,
+	primary key(receiptId, upc));
+ 
+ 
+drop table if exists customer;
+create table customer
+	(cid varchar(3) not null,
+	name varchar(40),
+	password varchar(40),
+	phone varchar(12),
+	address varchar(40),
+    primary key(cid));
+ 
 
  
 drop table if exists returns;
- 
 create table returns
-	(retID int(10),
-	returnDate date not null,
-	receiptId int(10) not null,
-	PRIMARY KEY(retID));
+	(retID int(10) not null auto_increment,
+	returnDate date,
+	receiptId int(10),
+	primary key(retID));
 
  
-drop table if exists returnItem;
- 
+drop table if exists returnItem; 
 create table returnItem
-	(retID int(10),
+	(retID int(10) not null,
 	upc char(37) not null,
-	quantity int not null,
-	PRIMARY KEY(retID, upc));
+	quantity int,
+	primary key(retID, upc));
+ 
+insert into item 
+values('001', 'cd1', 'cd', 'rock', 'Pearson', 1996, 20.5, 10
+ );
+ 
+insert into item 
+values('002', 'cd2', 'cd', 'pop', 'Reed Elsevier', 2005, 19.99, 5
+ );
+ 
+insert into item 
+values('003', 'cd3', 'cd', 'rap', 'Wolters Kluwer', 2011, 10.5, 8
+ );
+ 
+insert into item 
+values('004', 'cd4', 'cd', 'country', 'Wolters Kluwer', 2011, 10.5, 8
+ );
+ 
+insert into item 
+values('005', 'cd5', 'cd', 'classical', 'Random House	', 2013, 19, 7
+ );
+ 
+insert into item 
+values('006', 'cd6', 'cd', 'new age', 'Reed Elsevier	', 2005, 30, 6
+ );
+   
+insert into item 
+values('007', 'cd7', 'cd', 'instrumental', 'Reed Elsevier	', 2005, 30, 6
+ );
 
+insert into item 
+values('008', 'cd8', 'cd', 'rock', 'Pearson', 1996, 20.5, 10
+ );
+ 
+insert into item 
+values('009', 'cd9', 'cd', 'pop', 'Reed Elsevier', 2005, 19.99, 5
+ );
+ 
+insert into item 
+values('010', 'cd10', 'cd', 'rap', 'Wolters Kluwer', 2011, 10.5, 8
+ );
+ 
+insert into item 
+values('011', 'cd11', 'cd', 'country', 'Wolters Kluwer', 2011, 10.5, 8
+ );
+ 
+insert into item 
+values('012', 'cd12', 'cd', 'classical', 'Random House	', 2013, 19, 7
+ );
+ 
+insert into item 
+values('013', 'cd13', 'cd', 'new age', 'Reed Elsevier	', 2005, 30, 6
+ );
+   
+insert into item 
+values('014', 'cd14', 'cd', 'instrumental', 'Reed Elsevier	', 2005, 30, 6
+ );
+ 
+ insert into item 
+values('015', 'dvd1', 'dvd', 'rock', 'Pearson', 1996, 20.5, 10
+ );
+ 
+insert into item 
+values('016', 'dvd2', 'dvd', 'pop', 'Reed Elsevier', 2005, 19.99, 5
+ );
+ 
+insert into item 
+values('017', 'dvd3', 'dvd', 'rap', 'Wolters Kluwer', 2011, 10.5, 8
+ );
+ 
+insert into item 
+values('018', 'dvd4', 'dvd', 'country', 'Random House	', 2013, 19, 7
+ );
+ 
+insert into item 
+values('019', 'dvd5', 'dvd', 'classical', 'Reed Elsevier	', 2005, 30, 6
+ );
+ 
+insert into item 
+values('020', 'dvd6', 'dvd', 'new age', 'Reed Elsevier	', 2005, 30, 6
+ );
+ 
+ insert into item 
+values('021', 'dvd7', 'dvd', 'instrumental', 'Reed Elsevier', 2005, 19.99, 5
+ );
+ 
+insert into item 
+values('022', 'dvd8', 'dvd', 'rock', 'Wolters Kluwer', 2011, 10.5, 8
+ );
+ 
+insert into item 
+values('023', 'dvd9', 'dvd', 'pop', 'Random House	', 2013, 19, 7
+ );
+ 
+insert into item 
+values('024', 'dvd10', 'dvd', 'rap', 'Reed Elsevier	', 2005, 30, 6
+ );
+ 
+insert into item 
+values('025', 'dvd11', 'dvd', 'country', 'Reed Elsevier	', 2005, 30, 6
+ );
+ 
+insert into item 
+values('026', 'dvd12', 'dvd', 'classical', 'Reed Elsevier	', 2005, 30, 6
+ );
+ 
+insert into item 
+values('027', 'dvd13', 'dvd', 'new age', 'Reed Elsevier	', 2005, 30, 6
+ );
+ 
+insert into item 
+values('028', 'dvd14', 'dvd', 'instrumental', 'Reed Elsevier	', 2005, 30, 6
+ );
 
- 
-insert into item 
-values('4a9d51b3-8c71-4546-9974-70029adcfc6a', 'Criminal Of Wood', 'Science fiction', 'country', 'Pearson', 1996, 20.5, 10
+insert into leadsinger 
+values('001', 'singer1'
  );
  
-insert into item 
-values('24f7eb56-b006-4a0e-b4e6-5c0091f368dc', 'Prince In The Mountains', 'Novel', 'classical', 'Reed Elsevier', 2005, 19.99, 5
+insert into leadsinger 
+values('002', 'singer2'
  );
  
-insert into item 
-values('4d8be8fb-4ccf-4ac9-a037-e89d5e006e32', 'Spiders With Sins', 'Fiction', 'new age', 'Wolters Kluwer', 2011, 10.5, 8
+insert into leadsinger 
+values('003', 'singer3'
  );
  
-insert into item 
-values('e2f8561c-d2d0-440d-b662-8ca5cf162274', 'Foreigners Of Hope', 'Poem', 'rock', 'Random House	', 2013, 19, 7
+insert into leadsinger 
+values('004', 'singer4'
  );
  
-insert into item 
-values('1d2fc2cc-7ecf-40a8-b404-9b062582a332', 'Rise Of Dawn', 'Drama', 'rock', 'Reed Elsevier	', 2005, 30, 6
+insert into leadsinger 
+values('005', 'singer5'
+ );
+ 
+insert into leadsinger 
+values('006', 'singer6'
+ );
+ 
+insert into leadsinger 
+values('007', 'singer7'
+ );
+ 
+insert into hasSong 
+values('001', 'cd1'
+ );
+ 
+insert into hasSong 
+values('005', 'cd2'
+ );
+ 
+insert into hasSong 
+values('009', 'cd3'
+ );
+ 
+insert into hasSong 
+values('013', 'cd13'
+ );
+ 
+insert into hasSong 
+values('017', 'cd14'
  );
 
-insert into leadsinger 
-values('4a9d51b3-8c71-4546-9974-70029adcfc6a', 'singer1'
+insert into hasSong 
+values('021', 'cd15'
  );
  
-insert into leadsinger 
-values('24f7eb56-b006-4a0e-b4e6-5c0091f368dc', 'singer1'
+insert into hasSong 
+values('025', 'cd16'
  );
  
-insert into leadsinger 
-values('4d8be8fb-4ccf-4ac9-a037-e89d5e006e32', 'singer2'
- );
- 
-insert into leadsinger 
-values('e2f8561c-d2d0-440d-b662-8ca5cf162274', 'singer2'
- );
- 
-insert into leadsinger 
-values('1d2fc2cc-7ecf-40a8-b404-9b062582a332', 'singer3'
- );
+
+insert into purchaseItem
+values(1, '001', 5	
+);
+
+insert into purchaseItem
+values(2, '013', 2	
+);
+
+insert into purchaseItem
+values(6, '020', 7	
+);
+
+insert into purchaseItem
+values(9, '005', 10	
+);
 
 insert into customer
 values('001', 'Bennet Abraham', '12345', 
@@ -153,5 +303,13 @@ values('009', 'Stringer Dirk', '12345',
 insert into customer
 values('010', 'Tony Allen', '12345', 
 '425-843-2891', '3672 Alto Av. Oakland CA');
+
+insert into returnItem
+values(1, '020', 1 
+);
+
+insert into returnItem
+values(2, '005', 2
+);
 
 commit;
