644fa2c0a77db0b6b11a86eaae1d03e1faba14ec
Alex Tan <tanalex1@gmail.com>
2014-11-23 01:49:15 -0800

    salesreport.php done

--- salesreport.php
+++ salesreport.php
@@ -24,11 +24,9 @@ Hence, each page is broken down into three parts:
 
 	// Perform requested operations from HTML form here
 	if ($_SERVER["REQUEST_METHOD"] == "GET") {
-		if (isset($_GET["exactdate"])) {
+		if (isset($_GET["date"])) {
 			global $items, $con;
-			$items = getSalesReport($con, $_GET["exactdate"], "exact");
-		} else if (isset($_GET["startdate"]) && isset($_GET["enddate")) {
-			// do ranged date stuff
+			$items = getSalesReport($con, $_GET["date"]);
 		}
 	}
 	
@@ -65,7 +63,7 @@ Hence, each page is broken down into three parts:
 	</script>
 
 	<!-- Page title -->
-	<title> View Cart </title>
+	<title>Daily Sales Report</title>
 </head>
 
 <body>
@@ -80,7 +78,6 @@ Hence, each page is broken down into three parts:
 
 			<!-- Designated area for all error/success messages (top of page) -->
 			<div class="messages">
-				<!-- heading -->
 				Error/Success Messages:<br>
 
 				<!-- Error/Success messages (if any) -->
@@ -89,14 +86,15 @@ Hence, each page is broken down into three parts:
 
 			<div class="content">
 			
-				<div class="header">SALES REPORT</div>
+				<div class="heading">SALES REPORT</div>
 				<form action="salesreport.php" method="get">
 					<table>
-						<tr><td align="right">Date</td><td align="left"><input type="text" id="datepicker" name="exactdate" value="<?php echo (isset($_GET["date"]) ? $_GET["date"] : "" );?>"></td><td><button type="submit">Apply</button></td></tr>
+						<tr><td align="right" style="padding-right:5px;">Date</td><td align="left"><input type="text" id="datepicker" name="date" value="<?php echo (isset($_GET["date"]) ? $_GET["date"] : "" );?>"></td><td><button type="submit">Apply</button></td></tr>
 					</table>
-				</form
+				</form>
+				<br>
 				<!-- Items -->
-				<?php global $items; showSalesReport($items); ?>
+				<?php if (isset($_GET["date"])) { global $items; showSalesReport($items); } ?>
 
 			</div>
 		</div>
@@ -108,15 +106,11 @@ Hence, each page is broken down into three parts:
 	// MODEL
 	// ===============
 
-	function getSalesReport($con, $date, $queryType) {
+	function getSalesReport($con, $date) {
 		//$mysql_date = date( 'Y-m-d', strtotime($date) );
 
-		if ($queryType = "exact") {
-			$statement = $con->prepare("SELECT item.upc, SUM(quantity), title, type, category, company, year, price, stock FROM purchase INNER JOIN purchaseitem ON purchase.receiptId=purchaseitem.receiptId INNER JOIN item ON purchaseitem.upc=item.upc WHERE purchaseDate = STR_TO_DATE(?, '%m/%d/%Y') GROUP BY item.upc");
-			$statement->bind_param("s", $date);
-		} else if ($queryType = "range") {
-			// do date range query
-		}
+		$statement = $con->prepare("SELECT item.upc, category, price, SUM(purchaseitem.quantity), price*SUM(purchaseitem.quantity) FROM purchase INNER JOIN purchaseitem ON purchase.receiptId=purchaseitem.receiptId INNER JOIN item ON purchaseitem.upc=item.upc WHERE purchaseDate = STR_TO_DATE(?, '%m/%d/%Y') GROUP BY item.upc ORDER BY category");
+		$statement->bind_param("s", $date);
 
 		$statement->execute();
 		$result = $statement->get_result();
@@ -124,11 +118,51 @@ Hence, each page is broken down into three parts:
 	}
 	
 	function showSalesReport($items) {
-		echo "<pre>";
+		echo "<table style='width:100%; text-align:center;'>";
+		echo "<tr style=\"font-weight: bold\">";
+		echo "<th>UPC</th>";
+		echo "<th>Category</th>";
+		echo "<th>Price</th>";
+		echo "<th>Units</th>";
+		echo "<th>Total Value</th>";
+
+		$currentCategory = "";
+		$categoryCount = 0;
+		$categoryTotal = 0;
+		
 		while ($row = mysqli_fetch_array($items, MYSQL_ASSOC)) {
-			print_r($row);
+			if ($currentCategory != $row["category"]) {
+				if ($currentCategory != "")
+					showCategoryTotal($categoryCount, $categoryTotal);
+				$currentCategory = $row["category"];
+				$categoryCount = 0;
+				$categoryTotal = 0;
+			}
+			
+			$categoryCount += $row["SUM(purchaseitem.quantity)"];
+			$categoryTotal += $row["price*SUM(purchaseitem.quantity)"];
+			
+			echo "<tr>";
+			echo "<td>" . $row["upc"] . "</td>";
+			echo "<td>" . $row["category"] . "</td>";
+			echo "<td>" . number_format($row["price"],2) . "</td>";
+			echo "<td>" . $row["SUM(purchaseitem.quantity)"] . "</td>";
+			echo "<td>" . number_format($row["price*SUM(purchaseitem.quantity)"],2) . "</td>";
+			echo "</tr>";
 		}
-		echo "</pre>";
+		
+		showCategoryTotal($categoryCount, $categoryTotal);
+		
+		echo "</table>";
+	}
+	
+	function showCategoryTotal($count, $total) {
+		echo "<tr>";
+		echo "<td colspan=\"2\"><td style=\"font-weight:bold;\">Total</td>";
+		echo "<td>" . $count . "</td>";
+		echo "<td>" . number_format($total,2) . "</td>";
+		echo "</tr>";
+		echo "<tr class=\"blank_row\"><td colspan=\"5\"></td></tr>";
 	}
 
 ?>
